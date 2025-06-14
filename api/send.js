4aaeea982cb02f3528cb26c9b07e10221a84edae
macromapinho <clempouille78@gmail.com>
2025-06-14 23:22:39 +0700

    Update send.js

--- api/send.js
+++ api/send.js
@@ -1,10 +1,22 @@
 const nodemailer = require('nodemailer');
 
 module.exports = async (req, res) => {
+  // 1. Autoriser les requêtes CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  // 2. Répondre aux pré-vols (CORS preflight)
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  // 3. Rejeter les autres méthodes que POST
   if (req.method !== 'POST') {
     return res.status(405).send({ message: 'Only POST requests allowed' });
   }
 
+  // 4. Ensuite ton code existant reste inchangé :
   const { firstName, lastName, email, subject, message } = req.body;
 
   let to = 'contact@ls2pavocats.fr';
